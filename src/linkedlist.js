4b5718b1722797075021267770bacf4d43e3877e
deepend15 <170750487+deepend15@users.noreply.github.com>
2025-06-07 16:28:19 -0400

    Refactor code & add additional methods

--- src/linkedlist.js
+++ src/linkedlist.js
@@ -6,11 +6,19 @@ class LinkedList {
     }
   };
 
-  findLastNode(obj) {
-    if (obj.nextNode === null) {
-      return obj;
-    } else {
-      return this.findLastNode(obj.nextNode);
+  head() {
+    if (!this.value) return null;
+    else return this;
+  }
+
+  tail() {
+    if (!this.value) return null;
+    else {
+      let currentNode = this;
+      while (currentNode.nextNode !== null) {
+        currentNode = currentNode.nextNode;
+      }
+      return currentNode;
     }
   }
 
@@ -20,40 +28,82 @@ class LinkedList {
       this.nextNode = null;
     } else {
       let newNode = new LinkedList.Node(value);
-      if (this.nextNode === null) {
-        this.nextNode = newNode;
-      } else {
-        const lastNode = this.findLastNode(this.nextNode);
-        lastNode.nextNode = newNode;
-      }
+      let tail = this.tail();
+      tail.nextNode = newNode;
     }
   }
 
   prepend(value) {
-    let newNode = new LinkedList.Node(this.value, this.nextNode);
-    this.value = value;
-    this.nextNode = newNode;
+    if (!this.value) {
+      this.value = value;
+      this.nextNode = null;
+    } else {
+      let newNode = new LinkedList.Node(this.value, this.nextNode);
+      this.value = value;
+      this.nextNode = newNode;
+    }
+  }
+
+  size() {
+    if (!this.value) return 0;
+    else {
+      let list = this;
+      let counter = 1;
+      while (list.nextNode !== null) {
+        list = list.nextNode;
+        counter += 1;
+      }
+      return counter;
+    }
+  }
+
+  at(index) {
+    if (index < 0 || index > this.size() - 1) {
+      return undefined;
+    } else {
+      let counter = 0;
+      let returnedNode;
+      for (let currentNode = this; ; currentNode = currentNode.nextNode) {
+        if (counter === index) {
+          returnedNode = currentNode;
+          break;
+        } else {
+          counter += 1;
+        }
+      }
+      return returnedNode;
+    }
+  }
+
+  pop() {
+    if (!this.value) return;
+    else if (this.size() === 1) {
+      delete this.value;
+      delete this.nextNode;
+    } else {
+      let penultimateNode = this.at(this.size() - 2);
+      penultimateNode.nextNode = null;
+    }
+  }
+
+  contains(value) {
+    let response = false;
+    if (!list.value) {
+      return response;
+    } else {
+      for (let currentNode = this; ; currentNode = currentNode.nextNode) {
+        if (currentNode.value === value) {
+          response = true;
+          break;
+        }
+        if (currentNode.nextNode === null) {
+          break;
+        }
+      }
+      return response;
+    }
   }
 }
 
 const list = new LinkedList();
-console.log(list);
 list.append(1);
-console.log(list);
-list.append(2);
-console.log(list);
-console.log(list.nextNode);
-list.append(3);
-list.append(4);
-list.append(5);
-list.append(6);
-console.log(list.nextNode.nextNode.nextNode.nextNode.nextNode);
-list.prepend(0);
-console.log(list);
-console.log(list.nextNode);
-console.log(list.nextNode.nextNode.nextNode.nextNode.nextNode.nextNode);
-list.prepend(-1);
-list.prepend(-2);
-list.prepend(-3);
-console.log(list);
-console.log(list.nextNode.nextNode.nextNode.nextNode.nextNode);
